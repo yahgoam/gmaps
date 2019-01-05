c2f5d6616c390ea9f19a732354f343b230bcb653
Yahgo <yahgostm@hotmail.com>
2019-01-05 10:10:45 -0300

    primeiro

---
+++ db/seeds.rb
@@ -0,0 +1,5 @@
+Place.create!([
+{ "name": "Buckingham Palace", "latitude": "51.501564","longitude": "-0.141944"},
+{ "name": "Westminster Abbey", "latitude": "51.499581", "longitude": "-0.127309"},
+{ "name": "Big Ben", "latitude": "51.500792", "longitude": "-0.124613"}
+])
